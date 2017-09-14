e4231c2bdc2364ba3b08b60a6dd3fc82292700cc
Mauricio Faccin <mauricio.herbstrith@gmail.com>
2017-09-14 01:14:45 -0300

    ajustes

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.get('/removeAll', routes.removeAll);
 app.post('/save', routes.saveTodo);
 app.get('/getKeys', routes.getKeys);
 app.get('/remove', routes.removeTodo);
-/*app.listen(4000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});*/
+//app.listen(4000, function(){
+//  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+//});
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
 	console.log('Servidor Inicializado na Porta', app.get('port'));
