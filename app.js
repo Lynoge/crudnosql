7cf4611be0772311ddd162a9ca106d2434d4dc3b
Mauricio Faccin <mauricio.herbstrith@gmail.com>
2017-09-14 02:12:52 -0300

    assaa

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.get('/remove', routes.removeTodo);
 //  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 //});
 
-app.listen(app.get('port'), function() {
-	console.log('Servidor Inicializado na Porta', app.get('port'));
+app.listen(5000, function() {
+	console.log('Servidor Inicializado na Porta', 5000);
 });
